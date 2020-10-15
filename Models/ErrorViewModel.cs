265f6f3ab6df77e7fb302b44b9bc99414f39dafb
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2020-10-15 13:32:27 -0500

    project initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApps_LogicApps.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
